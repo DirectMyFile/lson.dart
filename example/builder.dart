3c0c3735c9b7029a96fdb653fde39931c6f17a16
Kenneth Endfinger <kaendfinger@gmail.com>
2015-02-23 19:39:20 -0500

    Formatting

--- example/builder.dart
+++ example/builder.dart
@@ -1,9 +1,13 @@
 import "package:lson/lson.dart";
 
 void main() {
-  var builder = new LsonTokensBuilder();
-  
-  builder.openCurlyBrace().string("Hello").colon().boolean(true).closeCurlyBrace();
-  
-  print(builder.toString());
+  print(
+      new LsonTokensBuilder()
+        .openCurlyBrace()
+        .string("Hello")
+        .colon()
+        .boolean(true)
+        .closeCurlyBrace()
+        .toString()
+  );
 }
